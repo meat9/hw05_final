d63e7f68053e8149783235beae3456d34343dcac
Aleksandr Zhikhar <meat9@yandex.ru>
2020-09-17 19:03:30 +0000

    cloud

--- yatube/settings.py
+++ yatube/settings.py
@@ -23,13 +23,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '532am#bi-r5qx2q5=1ps)&sr(hqd)9ez_%%_(d367^18oo1r%v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
         "localhost",
         "127.0.0.1",
         "[::1]",
+	"84.201.179.160",
         "testserver",
+
 ]
 
 # Application definition
